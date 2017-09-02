766d6ef75acce21d50bf5523b6238f8aa1cbab8a
watsonsong <watsonsong@foxmail.com>
2017-09-02 11:04:47 +0800

    Add the NDK_ROOT~~

--- build_arm.sh
+++ build_arm.sh
@@ -1,11 +1,14 @@
 cd luajit-2.1/src
 
 # Android/ARM, armeabi-v7a (ARMv7 VFP), Android 4.0+ (ICS)
-NDK=D:/android-ndk-r10e
+NDK=$NDK_ROOT
+if [ ! -n "$NDK" ]; then
+    NDK=D:/android-ndk-r10e
+fi
 NDKABI=19
 NDKVER=$NDK/toolchains/arm-linux-androideabi-4.9
 NDKP=$NDKVER/prebuilt/windows-x86_64/bin/arm-linux-androideabi-
-NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm" 
+NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
 NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
 
 make clean
@@ -14,7 +17,7 @@ cp ./libluajit.a ../../android/jni/libluajit.a
 make clean
 
 cd ../../android
-ndk-build clean APP_ABI="armeabi-v7a,x86"
-ndk-build APP_ABI="armeabi-v7a"
+$NDK/ndk-build clean APP_ABI="armeabi-v7a,x86"
+$NDK/ndk-build APP_ABI="armeabi-v7a"
 cp libs/armeabi-v7a/libtolua.so ../Plugins/Android/libs/armeabi-v7a
-ndk-build clean APP_ABI="armeabi-v7a,x86"
+$NDK/ndk-build clean APP_ABI="armeabi-v7a,x86"
